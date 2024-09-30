65aa8754ebd001d1e92c51def220e866025d1247
danielfdez17 <daniel17dev@gmail.com>
2024-09-30 17:09:13 +0200

    terminar de dar estilo y añadir la funcionalidad

---
+++ Ejercicio3/js/scripts.js
@@ -0,0 +1,7 @@
+const nombre = document.getElementById("nombre");
+const primerApellido = document.getElementById("primer_apellido");
+const segundoApellido = document.getElementById("segundo_apellido");
+const direccion = document.getElementById("direccion");
+const email = document.getElementById("email");
+const contrasenya = document.getElementById("contrasenya");
+const prefieroNoDecirlo = document.getElementById()
