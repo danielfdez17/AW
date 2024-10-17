ad6c7774ea8d03a293ed2b56f0a21e1bb7694e65
joseatort <joseantoniotortosatic@gmail.com>
2024-10-17 17:45:06 +0200

    Get funciona

--- calculator_project/app.js
+++ calculator_project/app.js
@@ -4,36 +4,24 @@ const path = require('path')
 const calculadora = require('./calculos')
 const app = express();
 
-
 // Middlewares generales
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-
-// Configuración del motor de vistas (EJS)
-app.post('/calculate', (req, res) => {
-    res.render('index');
+app.get('/calculate', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));  // Envía el archivo estático
 });
-
 
 // Manejo de errores 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Manejador de errores
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 app.listen(3000, () => 
 {
-   console.log('Servidor en marcha'); 
+    console.log('Servidor en marcha'); 
 })
+
 
 module.exports = app;
