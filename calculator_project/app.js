2ca4b62d828766ef5271c8a84e60cf8cd1b6b509
joseatort <joseantoniotortosatic@gmail.com>
2024-10-17 20:30:05 +0200

    Calculadora hecha, falta mostrar resultado

--- calculator_project/app.js
+++ calculator_project/app.js
@@ -1,16 +1,38 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path')
-const calculadora = require('./calculos')
+const calculadora = require('./calculos');
+const exp = require('constants');
 const app = express();
 
 // Middlewares generales
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/calculate', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));  // Envía el archivo estático
 });
+
+app.post('/calculate', (req, res) => {
+  let resultado = 0;
+
+  let primer_valor = parseFloat(req.body.firstValue);
+  let segundo_valor = parseFloat(req.body.secondValue);
+
+  if(req.body.operator === "+")
+    resultado = calculadora.sum(primer_valor, segundo_valor);
+  else if(req.body.operator === "-")
+    resultado = calculadora.subtract(primer_valor, segundo_valor);
+  else if(req.body.operator === "*")
+    resultado = calculadora.multiply(primer_valor, segundo_valor);
+  else if(req.body.operator === "/")
+    resultado = calculadora.divide(primer_valor, segundo_valor);
+
+  res.json({ resultado: resultado });
+});
+
+
 
 // Manejo de errores 404
 app.use(function(req, res, next) {
--- calculator_project/public/scripts/script.js
+++ calculator_project/public/scripts/script.js
@@ -1,1 +1,29 @@
-console.log("")
+"use strict";
+
+var firstValue;
+var operator;
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.querySelectorAll('button').forEach(button => {
+        button.addEventListener('click', function() {
+            const value = this.value;
+
+            if (this.name === 'data-number') {
+                document.getElementById('result').value += this.value;
+            } else if (this.name === 'data-opera') {    
+                firstValue = document.getElementById('result').value;
+                operator = value;
+                document.getElementById('result').value = 0;
+            } else if (this.name === 'data-delete') {
+                document.getElementById('result').value = 0;
+            } else if (this.name === 'data-igual') {
+                document.getElementById('firstValue').value = firstValue;
+                document.getElementById('operator').value = operator;
+                document.getElementById('secondValue').value = document.getElementById('result').value;
+                document.getElementById('formulario').submit();
+            }
+        });
+    });
+});
+
