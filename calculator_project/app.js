091c2fe66dd90bfba17fdb1965c0fb3096ab4220
danielfdez17 <daniel17dev@gmail.com>
2024-10-18 09:11:04 +0200

    display muestra resultado

--- calculator_project/app.js
+++ calculator_project/app.js
@@ -1,49 +1,46 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path')
-const calculadora = require('./calculos');
-const exp = require('constants');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const calculadora = require("./calculos");
+const exp = require("constants");
 const app = express();
+const port = 3000;
 
 // Middlewares generales
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/calculate', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));  // Envía el archivo estático
+app.get("/calculate", (req, res) => {
+  // Envía el archivo estático
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.post('/calculate', (req, res) => {
+app.post("/calculate", (req, res) => {
   let resultado = 0;
 
   let primer_valor = parseFloat(req.body.firstValue);
   let segundo_valor = parseFloat(req.body.secondValue);
 
-  if(req.body.operator === "+")
+  if (req.body.operator === "+")
     resultado = calculadora.sum(primer_valor, segundo_valor);
-  else if(req.body.operator === "-")
+  else if (req.body.operator === "-")
     resultado = calculadora.subtract(primer_valor, segundo_valor);
-  else if(req.body.operator === "*")
+  else if (req.body.operator === "*")
     resultado = calculadora.multiply(primer_valor, segundo_valor);
-  else if(req.body.operator === "/")
+  else if (req.body.operator === "/")
     resultado = calculadora.divide(primer_valor, segundo_valor);
 
   res.json({ resultado: resultado });
 });
 
-
-
 // Manejo de errores 404
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
-app.listen(3000, () => 
-{
-    console.log('Servidor en marcha'); 
-})
-
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto http://localhost:${port}`);
+});
 
 module.exports = app;
--- calculator_project/public/scripts/script.js
+++ calculator_project/public/scripts/script.js
@@ -3,27 +3,55 @@
 var firstValue;
 var operator;
 
-document.addEventListener("DOMContentLoaded", function() {
+function show(c) {
+  if ($("#display").val() !== "0") {
+    $("#display").val($("#display").val() + c);
+  } else {
+    $("#display").val(c);
+  }
+}
 
-    document.querySelectorAll('button').forEach(button => {
-        button.addEventListener('click', function() {
-            const value = this.value;
+function clearDisplay() {
+  $("#display").val("0");
+}
 
-            if (this.name === 'data-number') {
-                document.getElementById('result').value += this.value;
-            } else if (this.name === 'data-opera') {    
-                firstValue = document.getElementById('result').value;
-                operator = value;
-                document.getElementById('result').value = 0;
-            } else if (this.name === 'data-delete') {
-                document.getElementById('result').value = 0;
-            } else if (this.name === 'data-igual') {
-                document.getElementById('firstValue').value = firstValue;
-                document.getElementById('operator').value = operator;
-                document.getElementById('secondValue').value = document.getElementById('result').value;
-                document.getElementById('formulario').submit();
-            }
-        });
-    });
+function calculate() {
+  try {
+    var result = eval($("#display").val());
+    $("#display").val(result);
+  } catch (error) {
+    $("#result").text("Error");
+  }
+}
+
+$("#formulario").on("submit", (event) => {
+  event.preventDefault();
+  $.post("/calculate", $(this).serialize(), (data) => {
+    $("#display").text(`${data.result}`).show();
+  });
 });
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.querySelectorAll("button").forEach((button) => {
+//     button.addEventListener("click", function () {
+//       const value = this.value;
+
+//       if (this.name === "data-number") {
+//         document.getElementById("result").innerText =
+//           document.getElementById("result").value + this.value;
+//       } else if (this.name === "data-opera") {
+//         firstValue = document.getElementById("result").value;
+//         operator = value;
+//         document.getElementById("result").value = 0;
+//       } else if (this.name === "data-delete") {
+//         document.getElementById("result").value = 0;
+//       } else if (this.name === "data-igual") {
+//         document.getElementById("firstValue").value = firstValue;
+//         document.getElementById("operator").value = operator;
+//         document.getElementById("secondValue").value =
+//           document.getElementById("result").value;
+//         document.getElementById("formulario").submit();
+//       }
+//     });
+//   });
+// });
