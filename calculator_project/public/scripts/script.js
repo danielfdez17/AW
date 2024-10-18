922caf0fb57f74e2a07ed8b185aa0e56458713e1
joseatort <joseantoniotortosatic@gmail.com>
2024-10-18 22:09:35 +0200

    Logica keylistener

--- calculator_project/public/scripts/script.js
+++ calculator_project/public/scripts/script.js
@@ -11,6 +11,18 @@ function show(c) {
   }
 }
 
+function remove() {
+  if ($("#display").val() !== "0")
+  {
+    let value = $("#display").val();
+    let final = value.length - 1;
+    $("#display").val(value.slice(0, final));
+  }
+  
+  if ($("#display").val() == "")
+    $("#display").val("0");
+}
+
 function clearDisplay() {
   $("#display").val("0");
 }
@@ -30,6 +42,28 @@ $("#formulario").on("submit", (event) => {
     $("#display").text(`${data.result}`).show();
   });
 });
+
+
+document.addEventListener("keydown", (event) => 
+{
+  const key = event.key;
+  if((key >= 0 && key <= 9) || key == ".")
+    show(key);
+  else if (key == "+" || key == "*" || key == "/" || key == "-")
+  {
+    operator = key;
+    show(key);
+  }
+  else if (key == "c")
+    clearDisplay();
+  else if (key == "=" || key == "Enter")
+    calculate();
+  else if (event.key == "Backspace" || event.key == "Delete")
+    remove();
+})
+
+
+
 
 // document.addEventListener("DOMContentLoaded", function () {
 //   document.querySelectorAll("button").forEach((button) => {
