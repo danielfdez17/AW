7420ab04f2eccb3f36d73267e890834eab01fda9
danielfdez17 <daniel17dev@gmail.com>
2024-10-19 07:10:32 +0200

    cambios menores

--- calculator_project/public/scripts/script.js
+++ calculator_project/public/scripts/script.js
@@ -1,8 +1,5 @@
 "use strict";
 
-var firstValue;
-var operator;
-
 function show(c) {
   if ($("#display").val() !== "0") {
     $("#display").val($("#display").val() + c);
@@ -12,15 +9,13 @@ function show(c) {
 }
 
 function remove() {
-  if ($("#display").val() !== "0")
-  {
+  if ($("#display").val() !== "0") {
     let value = $("#display").val();
     let final = value.length - 1;
     $("#display").val(value.slice(0, final));
   }
-  
-  if ($("#display").val() == "")
-    $("#display").val("0");
+
+  if ($("#display").val() == "") $("#display").val("0");
 }
 
 function clearDisplay() {
@@ -43,49 +38,18 @@ $("#formulario").on("submit", (event) => {
   });
 });
 
-
-document.addEventListener("keydown", (event) => 
-{
+document.addEventListener("keydown", (event) => {
   const key = event.key;
-  if((key >= 0 && key <= 9) || key == ".")
+  if (
+    (key >= 0 && key <= 9) ||
+    key == "." ||
+    key == "+" ||
+    key == "*" ||
+    key == "/" ||
+    key == "-"
+  ) {
     show(key);
-  else if (key == "+" || key == "*" || key == "/" || key == "-")
-  {
-    operator = key;
-    show(key);
-  }
-  else if (key == "c")
-    clearDisplay();
-  else if (key == "=" || key == "Enter")
-    calculate();
-  else if (event.key == "Backspace" || event.key == "Delete")
-    remove();
-})
-
-
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   document.querySelectorAll("button").forEach((button) => {
-//     button.addEventListener("click", function () {
-//       const value = this.value;
-
-//       if (this.name === "data-number") {
-//         document.getElementById("result").innerText =
-//           document.getElementById("result").value + this.value;
-//       } else if (this.name === "data-opera") {
-//         firstValue = document.getElementById("result").value;
-//         operator = value;
-//         document.getElementById("result").value = 0;
-//       } else if (this.name === "data-delete") {
-//         document.getElementById("result").value = 0;
-//       } else if (this.name === "data-igual") {
-//         document.getElementById("firstValue").value = firstValue;
-//         document.getElementById("operator").value = operator;
-//         document.getElementById("secondValue").value =
-//           document.getElementById("result").value;
-//         document.getElementById("formulario").submit();
-//       }
-//     });
-//   });
-// });
+  } else if (key == "c") clearDisplay();
+  else if (key == "=" || key == "Enter") calculate();
+  else if (event.key == "Backspace" || event.key == "Delete") remove();
+});
