41ecd89bdf858267df79d9287e0c75eea880cf8d
danielfdez17 <daniel17dev@gmail.com>
2024-10-22 08:54:01 +0200

    comentario añadidos, faltaba use strict en app.js y añadido espacio antes del último important

--- calculator_project/app.js
+++ calculator_project/app.js
@@ -1,3 +1,6 @@
+"use strict";
+
+// Módulos importados y constantes globales
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -11,21 +14,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Envía el archivo estático para mostrar la calculadora
 app.get("/calculate", (req, res) => {
-  // Envía el archivo estático
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Recibe la petición del formulario para realizar la operación ingresada por el usuario
 app.post("/calculate", (req, res) => {
   let resultado = 0;
 
   let primer_valor = parseFloat(req.body.firstValue);
   let segundo_valor = parseFloat(req.body.secondValue);
 
-  if (req.body.operator === "+") resultado = calculadora.sum(primer_valor, segundo_valor);
-  else if (req.body.operator === "-") resultado = calculadora.subtract(primer_valor, segundo_valor);
-  else if (req.body.operator === "*") resultado = calculadora.multiply(primer_valor, segundo_valor);
-  else if (req.body.operator === "/") resultado = calculadora.divide(primer_valor, segundo_valor);
+  if (req.body.operator === "+")
+    resultado = calculadora.sum(primer_valor, segundo_valor);
+  else if (req.body.operator === "-")
+    resultado = calculadora.subtract(primer_valor, segundo_valor);
+  else if (req.body.operator === "*")
+    resultado = calculadora.multiply(primer_valor, segundo_valor);
+  else if (req.body.operator === "/")
+    resultado = calculadora.divide(primer_valor, segundo_valor);
 
   res.json({ resultado: resultado });
 });
@@ -35,6 +43,7 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+// El servidor se queda escuchando en el puerto 'port'
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto http://localhost:${port}`);
 });
--- calculator_project/public/scripts/script.js
+++ calculator_project/public/scripts/script.js
@@ -1,28 +1,39 @@
 "use strict";
 
+// Función que muestra el valor del caracter 'c' en el display
 function show(c) {
   let ultimoCaracter = $("#display").val().slice(-1);
   let displayValue = $("#display").val();
 
-  let ultimoNumero = displayValue.split(/[\+\-\*\/]/).pop().trim();
-  //Permite comenzar con decimales 0.x sin este if se vería .x sin el 0
-  if(c == "." && 
-    ($("#display").val().length == 1 && $("#display").val() === "0")) $("#display").val("0.")
+  let ultimoNumero = displayValue
+    .split(/[\+\-\*\/]/)
+    .pop()
+    .trim();
+  //Permite comenzar con decimales 0.x; Sin este if se vería .x sin el 0
+  if (
+    c == "." &&
+    $("#display").val().length == 1 &&
+    $("#display").val() === "0"
+  )
+    $("#display").val("0.");
   //Evitamos poder poner mas de 2 puntos en un numero que ya es decimal
-  else if((c == "." && parseFloat(ultimoNumero) % 1 !== 0) || (ultimoCaracter == "." && c == ".")) return;
-  else
-  {
-    //Sutiuimos el operador
-    if(/[+\-*/]/.test(ultimoCaracter) && /[+\-*/]/.test(c)) 
-      {
-        remove();
-        $("#display").val($("#display").val() + c);
-      }
-      else if (($("#display").val() !== "0")) $("#display").val($("#display").val() + c);
-      else $("#display").val(c);
+  else if (
+    (c == "." && parseFloat(ultimoNumero) % 1 !== 0) ||
+    (ultimoCaracter == "." && c == ".")
+  )
+    return;
+  else {
+    //Sustituimos el operador
+    if (/[+\-*/]/.test(ultimoCaracter) && /[+\-*/]/.test(c)) {
+      remove();
+      $("#display").val($("#display").val() + c);
+    } else if ($("#display").val() !== "0")
+      $("#display").val($("#display").val() + c);
+    else $("#display").val(c);
   }
 }
 
+// Función que elimina un caracter del display
 function remove() {
   if ($("#display").val() !== "0") {
     let value = $("#display").val();
@@ -33,10 +44,12 @@ function remove() {
   if ($("#display").val() == "") $("#display").val("0");
 }
 
+// Función que vacía el display
 function clearDisplay() {
   $("#display").val("0");
 }
 
+// Función que calcula el resultado de la operación ingresada por el usuario
 function calculate() {
   try {
     var result = eval($("#display").val());
@@ -46,6 +59,7 @@ function calculate() {
   }
 }
 
+// Función que calcula el resultado de la operación ingresada por el usuario utilizando JQuery
 $("#formulario").on("submit", (event) => {
   event.preventDefault();
   $.post("/calculate", $(this).serialize(), (data) => {
@@ -53,8 +67,8 @@ $("#formulario").on("submit", (event) => {
   });
 });
 
+// Se añaden key listeners por si el usuario prefiere operar con el teclado
 document.addEventListener("keydown", (event) => {
-  
   const key = event.key;
   if (
     (key >= 0 && key <= 9 && key != " ") ||
