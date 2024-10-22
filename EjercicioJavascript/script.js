a81a2c93d3bb5816b2fd52721677195ea9833687
joseatort <joseantoniotortosatic@gmail.com>
2024-10-22 21:13:19 +0200

    Comentado

--- EjercicioJavascript/script.js
+++ EjercicioJavascript/script.js
@@ -7,13 +7,17 @@ function conocermas()
 
     //Animacion
     setTimeout(async () => {
-        boton.style.display = "none"; // Oculta el botón después de 500ms
+        //Ocultamos el boton
+        boton.style.display = "none";
         let dinamico = document.getElementById("texto-dinamico");
 
+        //Movimiento
         document.getElementById("imagen").style.transform = "translateY(100px)";
         dinamico.style.transform = "translateY(100px)";
+        //Añadimos texto
         dinamico.innerText += '\n\nDesde controlar tu hogar inteligente hasta mejorar la productividad en el trabajo o los estudios, TechBoost es el compañero perfecto para quienes buscan optimizar su tiempo y experiencias. Con su diseño elegante y funcionalidades personalizables, se adapta a las necesidades específicas de cada usuario, revolucionando la conectividad y simplificando el acceso a la tecnología.';
 
+        //Animacion de vuelta
         await setTimeout(() => {
             document.getElementById("imagen").style.transform = "translateY(0px)";
             dinamico.style.transform = "translateY(0px)";
@@ -22,17 +26,20 @@ function conocermas()
     }, 300); // Esperar 300ms para ocultar el botón
 }
 
+//Colores claros
 const inicio_degradado_claro = "rgb(255, 237, 241)";
 const fin_degradado_claro = "rgb(207, 146, 238)";
 const text_color_claro = "rgb(20, 20, 28)";
 
+//Colores oscuros
 const inicio_degradado_oscuro = "rgb(0, 0, 0)";
 const fin_degradado_oscuro = "rgb(231, 207, 243)";
 const text_color_oscuro = "rgb(255, 237, 241)";
 
+//Ajustes de tema
 function ajustes_color(tipo_letra)
 {
-    const root = document.documentElement;
+    let root = document.documentElement;
     if(tipo_letra == "Claro")
     {
         root.style.setProperty('--inicio-degradado', inicio_degradado_claro);
@@ -53,6 +60,7 @@ function ajustes_color(tipo_letra)
     }
 }
 
+//Ajustes tamaño de letra
 function ajustes_texto(tipo_color)
 {
     if(tipo_color == "Normal")
@@ -64,10 +72,12 @@ function ajustes_texto(tipo_color)
 }   
 
 document.addEventListener("DOMContentLoaded", function() {
+    //Identificamos los elementos a los que queremos añadirle el EventListener
     document.querySelectorAll('.dropdown-menu ul').forEach(function(list) {
-
         list.querySelectorAll('.dropdown-item').forEach(function(item) {
-            
+
+            //Añadimos EventListener, para que el momento de hacer click sobre alguno  
+            //de estos elementos salte la funcion 
             item.addEventListener('click', function(event) {
                 // Evita que el dropdown se cierre al hacer clic
                 event.stopPropagation();
@@ -80,6 +90,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 // Añade la clase 'active' al elemento clicado
                 item.classList.add('active');
 
+                //Identificamos que lista es
                 if(list.id == "Tema")
                     ajustes_color(item.getAttribute("value"));
                 else if (list.id == "Letra")
@@ -87,5 +98,6 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         });
     });
+    //Inicialmente los ajustes corresponden al predeterminado
     ajustes_color("Predeterminado");
 });
