2886cf035a2798e081aeb51d59d6ae1932306aeb
danielfdez17 <daniel17dev@gmail.com>
2024-10-24 16:28:48 +0200

    hechas funciones eliminarProducto y leerProductos

---
+++ operaciones.js
@@ -0,0 +1,43 @@
+"use strict";
+const { pool } = require("./app.js");
+
+function insertarProducto(producto) {}
+function eliminarProducto(id, callback) {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      callback(err);
+    } else {
+      const sql = "UPDATE productos SET activo = 0 WHERE activo = ?";
+      connection.query(sql, [id], (err, rows) => {
+        connection.release();
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, filas);
+        }
+      });
+    }
+  });
+}
+function leerProductos() {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      callback(err);
+    } else {
+      const sql = "SELECT * FROM productos WHERE activo = 1";
+      connection.query(sql, (err, rows) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, rows);
+        }
+      });
+    }
+  });
+}
+
+module.exports = {
+  insertarProducto,
+  eliminarProducto,
+  leerProductos,
+};
