98be1a4cba5110d1033d7327991a464fc09682b5
joseatort <joseantoniotortosatic@gmail.com>
2024-10-24 16:32:21 +0200

    Pool

---
+++ app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// Módulos importados y constantes globales
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const calculadora = require("./calculos");
+const app = express();
+const port = 3000;
+const mysql = require("mysql2")
+
+// Database
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB
+});
+
+// Middlewares generales
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Manejo de errores 404
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// El servidor se queda escuchando en el puerto 'port'
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto http://localhost:${port}`);
+});
+
+module.exports = {app, pool};
