61f09f84961824cd7016529e89cb9dc20abc3e30
danielfdez17 <daniel17dev@gmail.com>
2024-11-01 08:42:12 +0100

    añadidos/adaptados comentarios del router

--- routers/router.js
+++ routers/router.js
@@ -2,13 +2,14 @@
 const express = require("express");
 const router = express.Router();
 const operaciones = require("../db/operaciones.js");
-const pool = require("../db/pool.js")
+const pool = require("../db/pool.js");
 
+// Redirige directamente a productos para evitar que el usuario tenga que escribir la URL completa
 router.get("/", (req, res) => {
   res.redirect("/productos");
 });
 
-// Envía el archivo estático para mostrar la calculadora
+// Renderiza la plantilla de productos con los productos leídos de la BD
 router.get("/productos", (req, res) => {
   operaciones.leerProductos(pool, (err, productos) => {
     if (err) {
@@ -19,7 +20,7 @@ router.get("/productos", (req, res) => {
   });
 });
 
-// Recibe la petición del formulario para realizar la operación ingresada por el usuario
+// Recibe la petición POST para añadir un nuevo producto a la BD
 router.post("/productos/nuevo", (req, res) => {
   const nuevo_producto = {
     nombre: req.body.nombre,
@@ -33,6 +34,7 @@ router.post("/productos/nuevo", (req, res) => {
   });
 });
 
+// Recibe la petición POST para desactivar un producto de la BD
 router.post("/productos/eliminar", (req, res) => {
   const id_producto = req.body.productoId;
   operaciones.eliminarProducto(pool, id_producto, (err, nombre) => {
