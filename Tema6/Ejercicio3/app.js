b8d4191f4925355f36627e3ec405318ca812acaa
danielfdez17 <daniel17dev@gmail.com>
2024-11-05 09:05:12 +0100

    Tema6/Ejercicio3 terminado

--- Tema6/Ejercicio3/app.js
+++ Tema6/Ejercicio3/app.js
@@ -1,12 +1,15 @@
 "use strict";
 const express = require("express");
 const path = require("path");
+const morgan = require("morgan");
 const router = require("./routers/router.js");
 const port = 3000;
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- Tema6/Ejercicio3/routers/router.js
+++ Tema6/Ejercicio3/routers/router.js
@@ -2,8 +2,46 @@
 const express = require("express");
 const router = express.Router();
 
+let nombre = "usuario";
+let password = "1234";
+let usuario_identificado = false;
+
+function identificacionRequerida(req, res, next) {
+  if (usuario_identificado) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+}
+
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("login");
+});
+
+router.get("/inicio", (req, res) => {
+  res.render("inicio");
+});
+
+router.get("/secreto", identificacionRequerida, (req, res) => {
+  res.render("secreto");
+});
+
+router.get("/otro_secreto", identificacionRequerida, (req, res) => {
+  res.render("otro_secreto");
+});
+
+router.get("/publico", (req, res) => {
+  res.render("publico");
+});
+
+router.post("/login", (req, res) => {
+  if (req.body.nombre === nombre && req.body.password === password) {
+    usuario_identificado = true;
+    res.redirect("/inicio");
+  } else {
+    usuario_identificado = false;
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
