7d6ad374c557a9258c05ae2f5f637250c7dc82e5
danielfdez17 <daniel17dev@gmail.com>
2024-11-05 09:35:47 +0100

    estructura Tema6/Ejercicio4 hecha

---
+++ Tema6/Ejercicio4/app.js
@@ -0,0 +1,27 @@
+"use strict";
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const router = require("./routers/router.js");
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(router);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error al iniciar el servidor: ${err}`);
+  } else {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+  }
+});
