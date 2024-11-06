b4246ac2c51287ee7cfc2827d03e3798cf893895
danielfdez17 <daniel17dev@gmail.com>
2024-11-06 16:50:26 +0100

    falta añadir conexión a BD

--- Tema6/Ejercicio4/routers/router.js
+++ Tema6/Ejercicio4/routers/router.js
@@ -6,4 +6,35 @@ router.get("/", (req, res) => {
   res.render("sumando1");
 });
 
+router.post("/primer_sumando", (req, res) => {
+  // * Guardar el valor en las cookies
+  res.render("sumando2");
+});
+
+router.get("/sumando2", (req, res) => {
+  res.render("sumando2");
+});
+
+router.post("/segundo_sumando", (req, res) => {
+  // * Recuperar el valor del primer sumando y sumarlo con el segundo
+  
+  const sum1 = 54;
+  const sum2 = 56;
+  res.render("resultado", {
+    sumando1: sum1,
+    sumando2: sum2,
+    resultado: sum1 + sum2,
+  });
+});
+
+// router.get("/resultado", (req, res) => {
+//   const sum1 = 54;
+//   const sum2 = 56;
+//   res.render("resultado", {
+//     sumando1: sum1,
+//     sumando2: sum2,
+//     resultado: sum1 + sum2,
+//   });
+// });
+
 module.exports = router;
