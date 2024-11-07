07b408d0c453bf817438a6fb0b30b97938b5fade
joseatort <joseantoniotortosatic@gmail.com>
2024-11-07 21:51:04 +0100

    Gestión de rutas solucionado y header

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 "use strict";
+
 const express = require("express");
-const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const createError = require('http-errors');
+const morgan = require('morgan');
 const path = require("path");
 const indexRouter = require("./routers/index.js");
 
@@ -14,13 +16,30 @@ app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(morgan("dev"));
 
+app.use('/', indexRouter);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(indexRouter);
 
-// TODO: añadir middleware de errores
+// Control error 404
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Control error 500
+app.use(function(req, res, next) {
+  next(createError(500));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', {error: err, message: err.message});
+});
+
 
 app.listen(port, (err) => {
   if (err) console.log(`Erro al iniciar el servidor: ${err}`);
--- routers/index.js
+++ routers/index.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const express = require("express");
+const router = express.Router();
+
+// Redirige directamente a productos para evitar que el usuario tenga que escribir la URL completa
+router.get('/', (req, res) => {
+    res.render("index");
+});
+  
+module.exports = router;
