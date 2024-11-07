2d4553e7656250d1dc3361311b987a6fd348ccdf
danielfdez17 <daniel17dev@gmail.com>
2024-11-07 08:25:35 +0100

    Tema6/Ejercicio4 terminado

--- Tema6/Ejercicio4/app.js
+++ Tema6/Ejercicio4/app.js
@@ -1,6 +1,8 @@
 "use strict";
 const express = require("express");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
 const path = require("path");
 const router = require("./routers/router.js");
 
@@ -11,10 +13,12 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(morgan("dev"));
+app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use(router);
 
--- Tema6/Ejercicio4/routers/router.js
+++ Tema6/Ejercicio4/routers/router.js
@@ -8,6 +8,8 @@ router.get("/", (req, res) => {
 
 router.post("/primer_sumando", (req, res) => {
   // * Guardar el valor en las cookies
+  console.log(req.body.primer_sumando);
+  res.cookie("sumando1", req.body.primer_sumando, { maxAge: 86400000 });
   res.render("sumando2");
 });
 
@@ -17,24 +19,30 @@ router.get("/sumando2", (req, res) => {
 
 router.post("/segundo_sumando", (req, res) => {
   // * Recuperar el valor del primer sumando y sumarlo con el segundo
-  
-  const sum1 = 54;
-  const sum2 = 56;
+  console.log(req.body.segundo_sumando);
+  res.cookie("sumando2", req.body.segundo_sumando, { maxAge: 86400000 });
+  res.redirect("/resultado");
+  //   const sum1 = 54;
+  //   const sum2 = 56;
+  //   res.render("resultado", {
+  //     sumando1: sum1,
+  //     sumando2: sum2,
+  //     resultado: sum1 + sum2,
+  //   });
+});
+
+router.get("/resultado", (req, res) => {
+  const sum1 = Number(req.cookies.sumando1);
+  const sum2 = Number(req.cookies.sumando2);
+  console.log(req.get("Cookie"));
+  res.clearCookie("sumando1");
+  res.clearCookie("sumando2");
+  console.log(req.get("Cookie"));
   res.render("resultado", {
     sumando1: sum1,
     sumando2: sum2,
     resultado: sum1 + sum2,
   });
 });
-
-// router.get("/resultado", (req, res) => {
-//   const sum1 = 54;
-//   const sum2 = 56;
-//   res.render("resultado", {
-//     sumando1: sum1,
-//     sumando2: sum2,
-//     resultado: sum1 + sum2,
-//   });
-// });
 
 module.exports = router;
