8a2509508b0d665bafd5ac1e153709d954ba1c0a
danielfdez17 <daniel17dev@gmail.com>
2024-11-09 16:56:12 +0100

    boton editar perfil funcional

---
+++ public/js/editarPerfil.js
@@ -0,0 +1,31 @@
+"use strict";
+const botonHabilitarEdicion = document.getElementById("habilitarEdicion");
+const botonCerrarEdicion = document.getElementById("cerrarEdicion");
+const inputNombre = document.getElementById("editarNombre");
+const inputCorreo = document.getElementById("editarCorreo");
+const inputContrasena = document.getElementById("editarContrasena");
+const inputTelefono = document.getElementById("editarTelefono");
+
+function habilitarEdicion() {
+  inputNombre.disabled = false;
+  inputCorreo.disabled = false;
+  inputContrasena.disabled = false;
+  inputTelefono.disabled = false;
+
+  inputNombre.value = inputNombre.placeholder;
+  inputCorreo.value = inputCorreo.placeholder;
+  inputContrasena.value = "";
+  inputTelefono.value = inputTelefono.placeholder;
+}
+
+function deshabilitarEdicion() {
+    inputNombre.disabled = true;
+    inputCorreo.disabled = true;
+    inputContrasena.disabled = true;
+    inputTelefono.disabled = true;
+    
+    inputNombre.value = "";
+    inputCorreo.value = "";
+    inputContrasena.value = "";
+    inputTelefono.value = "";
+}
