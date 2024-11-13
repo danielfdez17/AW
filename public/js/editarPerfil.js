c0c2fbf47257e33bce361c74478549da4c77f052
danielfdez17 <daniel17dev@gmail.com>
2024-11-13 09:26:39 +0100

    añadido control de datos en el cliente con JQuery (está en la carpeta de Google Drive que te compartí: Temario de otros años)

--- public/js/editarPerfil.js
+++ public/js/editarPerfil.js
@@ -1,29 +1,48 @@
 "use strict";
+const dominio = "@ucm.es";
 
 function habilitarEdicion() {
-    $("#editarNombre").prop("disabled", false);
-    $("#editarCorreo").prop("disabled", false);
-    $("#editarContrasena").prop("disabled", false);
-    $("#editarTelefono").prop("disabled", false);
+  $("#editarNombre").prop("disabled", false);
+  $("#editarCorreo").prop("disabled", false);
+  $("#editarContrasena").prop("disabled", false);
+  $("#editarTelefono").prop("disabled", false);
 
-    $("#editarNombre").prop("value", $("#editarNombre").prop("placeholder"));
-    $("#editarCorreo").prop("value", $("#editarCorreo").prop("placeholder"));
-    $("#editarContrasena").prop("value", "");
-    $("#editarTelefono").prop("value", $("#editarTelefono").prop("placeholder"));
-    
+  $("#editarNombre").prop("value", $("#editarNombre").prop("placeholder"));
+  $("#editarCorreo").prop("value", $("#editarCorreo").prop("placeholder"));
+  $("#editarContrasena").prop("value", "");
+  $("#editarTelefono").prop("value", $("#editarTelefono").prop("placeholder"));
 }
 
 function deshabilitarEdicion() {
-    $("#editarNombre").prop("disabled", true);
-    $("#editarCorreo").prop("disabled", true);
-    $("#editarContrasena").prop("disabled", true);
-    $("#editarTelefono").prop("disabled", true);
-    
-    $("#editarNombre").prop("value", "");
-    $("#editarCorreo").prop("value", "");
-    $("#editarContrasena").prop("value", "");
-    $("#editarTelefono").prop("value", "");
+  $("#editarNombre").prop("disabled", true);
+  $("#editarCorreo").prop("disabled", true);
+  $("#editarContrasena").prop("disabled", true);
+  $("#editarTelefono").prop("disabled", true);
+
+  $("#editarNombre").prop("value", "");
+  $("#editarCorreo").prop("value", "");
+  $("#editarContrasena").prop("value", "");
+  $("#editarTelefono").prop("value", "");
 }
 
-$("#habilitarEdicion").on("click", habilitarEdicion)
-$("#cerrarEdicion").on("click", deshabilitarEdicion)
+$("#habilitarEdicion").on("click", habilitarEdicion);
+$("#cerrarEdicion").on("click", deshabilitarEdicion);
+
+$("#formEditarPerfil button[type=submit]").on("click", (event) => {
+  if (/\d/.test($("#editarNombre").prop("value"))) {
+    alert("El campo nombre no puede contener números");
+  }
+  if (!/^\d+$/.test($("#editarTelefono").prop("value"))) {
+    alert("El campo teléfono solo puede contener números");
+  }
+  if (
+    $("#editarCorreo").prop("value").includes("@") &&
+    !$("#editarCorreo").prop("value").includes(dominio)
+  ) {
+    alert(`El correo debe terminar en ${dominio}`);
+  }
+  if ($("#editarContrasena").prop("value").length < 3) {
+    alert("La contraseña debe tener mínimo 4 caracteres");
+  }
+  event.preventDefault();
+});
