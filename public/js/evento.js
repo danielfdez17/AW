4044ef85a77fcede8489d7cb2e6fdd9a44353221
joseatort <joseantoniotortosatic@gmail.com>
2024-11-17 15:33:41 +0100

    Lógica botones editar

--- public/js/evento.js
+++ public/js/evento.js
@@ -1,40 +1,53 @@
 "use strict;"
 
-function habilitarEdicion() {
-    $("#editarNombre").prop("disabled", false);
-    $("#editarCorreo").prop("disabled", false);
-    $("#editarContrasena").prop("disabled", false);
-    $("#editarTelefono").prop("disabled", false);
-    $("#editarFacultad").prop("disabled", false);
+function habilitarEdicion(id_evento) {
+    // $("#editarNombre").prop("disabled", false);
+    // $("#editarCorreo").prop("disabled", false);
+    // $("#editarContrasena").prop("disabled", false);
+    // $("#editarTelefono").prop("disabled", false);
+    // $("#editarFacultad").prop("disabled", false);
   
-    $("#editarNombre").val($("#editarNombre").prop("placeholder"));
-    $("#editarCorreo").val($("#editarCorreo").prop("placeholder"));
-    $("#editarContrasena").val("");
-    $("#editarTelefono").val($("#editarTelefono").prop("placeholder"));
-    $("#editarTelefono").val($("#editarTelefono").prop("placeholder"));
+    // $("#editarNombre").val($("#editarNombre").prop("placeholder"));
+    // $("#editarCorreo").val($("#editarCorreo").prop("placeholder"));
+    // $("#editarContrasena").val("");
+    // $("#editarTelefono").val($("#editarTelefono").prop("placeholder"));
+    // $("#editarTelefono").val($("#editarTelefono").prop("placeholder"));
   
     //Botones
-    document.getElementById("guardar").style.display = "block";
-    document.getElementById("cancelarEdicion").style.display = "block";
-    document.getElementById("habilitarEdicion").style.display = "none";
-  }
-  
-function deshabilitarEdicion() {
-    $("#editarNombre").prop("disabled", true);
-    $("#editarCorreo").prop("disabled", true);
-    $("#editarContrasena").prop("disabled", true);
-    $("#editarTelefono").prop("disabled", true);
-    $("#editarFacultad").prop("disabled", true);
+    document.getElementById(`editarEvento${id_evento}`).classList.add('ocultar');
+    document.getElementById(`eliminarEvento${id_evento}`).classList.add('ocultar');
+    document.getElementById(`cancelarEvento${id_evento}`).classList.add('mostrar');
+    document.getElementById(`guardarEvento${id_evento}`).classList.add('mostrar');
 
-    $("#editarNombre").val("");
-    $("#editarCorreo").val("");
-    $("#editarContrasena").val("");
-    $("#editarTelefono").val("");
+
+    document.getElementById(`editarEvento${id_evento}`).classList.remove('mostrar');
+    document.getElementById(`eliminarEvento${id_evento}`).classList.remove('mostrar');
+    document.getElementById(`cancelarEvento${id_evento}`).classList.remove('ocultar');
+    document.getElementById(`guardarEvento${id_evento}`).classList.remove('ocultar');
+}
+  
+function deshabilitarEdicion(id_evento) {
+    // $("#editarNombre").prop("disabled", true);
+    // $("#editarCorreo").prop("disabled", true);
+    // $("#editarContrasena").prop("disabled", true);
+    // $("#editarTelefono").prop("disabled", true);
+    // $("#editarFacultad").prop("disabled", true);
+
+    // $("#editarNombre").val("");
+    // $("#editarCorreo").val("");
+    // $("#editarContrasena").val("");
+    // $("#editarTelefono").val("");
 
     //Botones
-    document.getElementById("guardar").style.display = "none";
-    document.getElementById("cancelarEdicion").style.display = "none";
-    document.getElementById("habilitarEdicion").style.display = "block";
+    document.getElementById(`editarEvento${id_evento}`).classList.add('mostrar');
+    document.getElementById(`eliminarEvento${id_evento}`).classList.add('mostrar');
+    document.getElementById(`cancelarEvento${id_evento}`).classList.add('ocultar');
+    document.getElementById(`guardarEvento${id_evento}`).classList.add('ocultar');
+
+    document.getElementById(`editarEvento${id_evento}`).classList.remove('ocultar');
+    document.getElementById(`eliminarEvento${id_evento}`).classList.remove('ocultar');
+    document.getElementById(`cancelarEvento${id_evento}`).classList.remove('mostrar');
+    document.getElementById(`guardarEvento${id_evento}`).classList.remove('mostrar');
 }
 
   
