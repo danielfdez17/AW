50daeef4d28b804f7898ee6623ba6c9c751e1b83
danielfdez17 <daniel17dev@gmail.com>
2024-11-19 10:21:36 +0100

    terminado ejercicio busqueda tesoro (falta refactorizar)

---
+++ Tema8/BuscaTesoros/js/scripts.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const TIEMPO_DESVANECIMIENTO = 2;
+const NUM_INTENTOS = 3;
+const IMG_SKULL = "/img/skull.png";
+const IMG_TESORO = "/img/tesoro.png";
+const IMG_ISLA = "/img/isla.png";
+
+// function asignarTesoroEsqueleto() {
+//   const NUM_ISLAS = $("#islas img").length;
+//   for (let i = 0; i < NUM_ISLAS; ++i) {
+//     cambiarImagen(
+//       $("#islas img").eq(i),
+//       Math.floor(Math.random() * 3) % 2 === 0 ? IMG_SKULL : IMG_TESORO
+//     );
+//   }
+// }
+
+$(() => {
+  $("button").addClass("btn btn-primary");
+  $("#islas").hide();
+  $("#fin").hide();
+  $("#comenzar").on("click", () => {
+    $("#inicio").hide();
+    $("#islas").show();
+    asignarTesoroEsqueleto();
+  });
+
+  let contador = NUM_INTENTOS;
+
+  function actualizarContador() {
+    $("#titulo p").html(`<p>${contador} intentos restantes</p>`);
+  }
+
+  let infoContador = $(`<p>${contador} intentos restantes</p>`);
+  $("#titulo").append(infoContador);
+
+  function cambiarImagen(isla, imagen) {
+    isla.on("click", () => {
+      isla.prop("src", imagen);
+      contador--;
+      actualizarContador();
+      if (imagen === IMG_TESORO) {
+        terminar(true);
+      } else if (contador === 0) {
+        terminar(false);
+      }
+    });
+  }
+
+  function asignarTesoroEsqueleto() {
+    const NUM_ISLAS = $("#islas img").length;
+    for (let i = 0; i < NUM_ISLAS; ++i) {
+      cambiarImagen(
+        $("#islas img").eq(i),
+        Math.floor(Math.random() * 3) % 2 === 0 ? IMG_SKULL : IMG_TESORO
+      );
+    }
+  }
+
+  function terminar(ganar) {
+    $("#mensaje").text(`Fin del juego: has ${ganar ? "ganado" : "perdido"}`);
+    $("#fin").show();
+    $("#islas img").prop("onclick", null).off("click");
+    $("#restart").on("click", () => {
+      contador = NUM_INTENTOS;
+      actualizarContador();
+      $("#islas").hide();
+      $("#fin").hide();
+      $("#inicio").show();
+      $("#islas img").prop("src", IMG_ISLA);
+      asignarTesoroEsqueleto();
+    });
+  }
+});
