3dac201116c22dc16441b074c1f5e65502306a4e
danielfdez17 <daniel17dev@gmail.com>
2024-11-21 20:11:24 +0100

    añadido control y lógica de duración de eventos en el controlador

--- controllers/eventos.js
+++ controllers/eventos.js
@@ -5,6 +5,20 @@ const daoEventos = new DAOEventos(pool);
 
 const { validationResult } = require("express-validator");
 
+const MIN_DURACION = "01:00";
+const MAX_DURACION = "08:00";
+const HORA_INICIO = "08:00";
+const HORA_FIN = "22:00";
+
+function checkHoraDuracion(hora, duracion) {
+  return (
+    HORA_INICIO <= hora &&
+    hora < HORA_FIN &&
+    MIN_DURACION <= duracion &&
+    duracion <= MAX_DURACION
+  );
+}
+
 class EventosController {
   crearEvento(req, res, next) {
     if (!validationResult(req).isEmpty())
@@ -22,23 +36,31 @@ class EventosController {
     const id_organizador = req.session.usuario.id;
     const capacidad_maxima = parseInt(capacidad_maxima_string);
     daoEventos.readEventoPorFecha(fecha, (eventos) => {
-      daoEventos.createEvento(
-        {
-          titulo,
-          descripcion,
-          fecha,
-          hora,
-          duracion,
-          ubicacion,
-          capacidad_maxima,
-          tipo_evento,
-          id_organizador,
-        },
-        (err) => {
-          if (err) next(err);
-          else res.redirect("/organizadores");
-        }
-      );
+      let sePuedeInsertar = true;
+      eventos.forEach((evento) => {
+        if (evento.hora + evento.duracion >= hora) sePuedeInsertar = false;
+      });
+      if (sePuedeInsertar) {
+        daoEventos.createEvento(
+          {
+            titulo,
+            descripcion,
+            fecha,
+            hora,
+            duracion,
+            ubicacion,
+            capacidad_maxima,
+            tipo_evento,
+            id_organizador,
+          },
+          (err) => {
+            if (err) next(err);
+            else res.redirect("/organizadores");
+          }
+        );
+      } else {
+        res.redirect("/organizadores");
+      }
     });
   }
 
@@ -67,23 +89,35 @@ class EventosController {
       id,
     } = req.body;
     const capacidad_maxima = parseInt(capacidad_maxima_string);
-    daoEventos.updateEvento(
-      {
-        titulo,
-        descripcion,
-        fecha,
-        hora,
-        duracion,
-        ubicacion,
-        capacidad_maxima,
-        tipo_evento,
-        id,
-      },
-      (err) => {
-        if (err) next(err);
-        res.redirect("/organizadores");
-      }
-    );
+    if (checkHoraDuracion(hora, duracion)) {
+      daoEventos.readEventoPorFecha(fecha, (eventos) => {
+        let sePuedeActualizar = true;
+        eventos.forEach((evento) => {
+          if (evento.hora + evento.duracion >= hora) sePuedeActualizar = false;
+        });
+        if (sePuedeActualizar) {
+          daoEventos.updateEvento(
+            {
+              titulo,
+              descripcion,
+              fecha,
+              hora,
+              duracion,
+              ubicacion,
+              capacidad_maxima,
+              tipo_evento,
+              id,
+            },
+            (err) => {
+              if (err) next(err);
+              res.redirect("/organizadores");
+            }
+          );
+        } else {
+          res.redirect("/organizadores");
+        }
+      });
+    }
   }
 }
 
