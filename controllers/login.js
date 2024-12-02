c5691f4d7d16a9e2bea8dd23fada3d3ba904e87c
danielfdez17 <daniel17dev@gmail.com>
2024-12-02 18:20:18 +0100

    probando encriptacion => falla

--- controllers/login.js
+++ controllers/login.js
@@ -3,6 +3,7 @@
 const DAOUsuarios = require("../db/daoUsuarios.js");
 const pool = require("../db/pool.js");
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
 
 const daoUsuarios = new DAOUsuarios(pool);
 
@@ -13,17 +14,43 @@ class LoginController {
     const { correoLogin, contrasenaLogin } = req.body;
     daoUsuarios.readUsuarioPorCorreo(correoLogin, (err, usuario) => {
       if (err) next(err);
-      if (usuario && usuario.contrasena === contrasenaLogin ) {
-        req.session.usuario = usuario;
-        req.session.auth = true;
-        res.setFlash({ message: "¡Has iniciado sesión con exito!", type: "exito" });
-        if(usuario.rol === "organizador")
-          res.redirect("/organizadores");
-        else
-          res.redirect("/asistentes");
-        
+      if (usuario) {
+        bcrypt.hash(contrasenaLogin, 10, (err, hash) => {
+          if (err) next(err);
+          else {
+            console.log(contrasenaLogin);
+            console.log(hash);
+            console.log(usuario.contrasena);
+            bcrypt.compare(hash, usuario.contrasena, (err, result) => {
+              if (err) next(err);
+              else {
+                if (result) {
+                  console.log(hash, " ", usuario.contrasena);
+                  req.session.usuario = usuario;
+                  req.session.auth = true;
+                  res.setFlash({
+                    message: "¡Has iniciado sesión con exito!",
+                    type: "exito",
+                  });
+                  if (usuario.rol === "organizador")
+                    res.redirect("/organizadores");
+                  else res.redirect("/asistentes");
+                } else {
+                  res.setFlash({
+                    message: "Error: correo y/o contraseña incorrectos",
+                    type: "error",
+                  });
+                  res.redirect("/");
+                }
+              }
+            });
+          }
+        });
       } else {
-        res.setFlash({ message: "Error: correo y/o contraseña incorrectos", type: "error" });
+        res.setFlash({
+          message: "Error: correo y/o contraseña incorrectos",
+          type: "error",
+        });
         res.redirect("/");
       }
     });
--- controllers/signUp.js
+++ controllers/signUp.js
@@ -3,7 +3,7 @@
 const DAOUsuarios = require("../db/daoUsuarios.js");
 const pool = require("../db/pool.js");
 const { validationResult } = require("express-validator");
-
+const bcrypt = require("bcrypt");
 const daoUsuarios = new DAOUsuarios(pool);
 
 class SignUpController {
@@ -12,21 +12,26 @@ class SignUpController {
       return res.status(400).json({ errors: validationResult(req).array() });
 
     let usuario = req.body;
-    if (req.file)
-      usuario.imagen = req.file.buffer ;
-    else
-      usuario.imagen = null;
-
-    daoUsuarios.createUsuario(usuario, (err, usuario) => {
-      if (err) next(err);
-      else {
-        req.session.usuario = usuario;
-        req.session.auth = true;
-        res.setFlash({ message: "¡Te has registrado correctamente!", type: "exito" });
-        if(usuario.rol === "organizador")
-          res.redirect("/organizadores");
-        else
-          res.redirect("/asistentes");
+    if (req.file) usuario.imagen = req.file.buffer;
+    else usuario.imagen = null;
+    bcrypt.hash(usuario.contrasena, 10, (err, hash) => {
+      if (err) {
+        next(err);
+      } else {
+        usuario.contrasena = hash;
+        daoUsuarios.createUsuario(usuario, (err, usuario) => {
+          if (err) next(err);
+          else {
+            req.session.usuario = usuario;
+            req.session.auth = true;
+            res.setFlash({
+              message: "¡Te has registrado correctamente!",
+              type: "exito",
+            });
+            if (usuario.rol === "organizador") res.redirect("/organizadores");
+            else res.redirect("/asistentes");
+          }
+        });
       }
     });
   }
