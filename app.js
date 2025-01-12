50f5ed5250f3e9c527dfb2dab8cbec080e04fef5
danielfdez17 <daniel17dev@gmail.com>
2025-01-12 13:35:47 +0100

    añadido express.urlencoded

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan("dev"));
 
@@ -44,7 +45,3 @@ app.listen(PORT, (err) => {
 });
 
 module.exports = app;
-
-
-
-
